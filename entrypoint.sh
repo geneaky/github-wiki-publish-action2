5d1d2fad00a008089f0db7c017c3cd55b7d5e184
geneaky <kaktus418@gmail.com>
2022-10-05 20:10:08 +0900

    change 8

--- entrypoint.sh
+++ entrypoint.sh
@@ -57,13 +57,15 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
     git pull "$GIT_REPOSITORY_URL"
 ) || exit 1
 
-cd $tmp_dir
+echo test start
 pwd
+echo test end
+
+rm -rf "$tmp_dir/$WIKI_PAGE_NAME.md"
 
 debug "Enumerating contents of $1"
 for file in $(find $1 -maxdepth 1 -type f -name '*.html' -execdir basename '{}' ';'); do
     debug "Copying $file"
-    rm -f "$tmp_dir/$WIKI_PAGE_NAME.md"
     mv "$1/$file" "$1/$WIKI_PAGE_NAME.md"
     mv "$1/$WIKI_PAGE_NAME.md" "$tmp_dir"
 done
