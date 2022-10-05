9e79188459cd9dc4981a847be03b126fb66cb24a
geneaky <kaktus418@gmail.com>
2022-10-05 19:46:26 +0900

    shell test 7

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,6 +40,11 @@ if [ -z "${WIKI_COMMIT_MESSAGE:-}" ]; then
     WIKI_COMMIT_MESSAGE='Automatically publish wiki'
 fi
 
+if [ -z "${WIKI_PAGE_NAME}"]; then
+    debug "WIKI PAGE NAME IS REQUIRED"
+    exit 1
+fi
+
 GIT_REPOSITORY_URL="https://${GH_PERSONAL_ACCESS_TOKEN}@${GITHUB_SERVER_URL#https://}/$GITHUB_REPOSITORY.wiki.git"
 
 debug "Checking out wiki repository"
@@ -55,9 +60,9 @@ tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
 debug "Enumerating contents of $1"
 for file in $(find $1 -maxdepth 1 -type f -name '*.html' -execdir basename '{}' ';'); do
     debug "Copying $file"
-    rm -f "$1/index.md"
-    mv "$1/$file" "$1/index.md"
-    mv "$1/index.md" "$tmp_dir"
+    rm -f "$1/$WIKI_PAGE_NAME.md"
+    mv "$1/$file" "$1/$WIKI_PAGE_NAME.md"
+    mv "$1/$WIKI_PAGE_NAME.md" "$tmp_dir"
 done
 
 debug "Committing and pushing changes"
